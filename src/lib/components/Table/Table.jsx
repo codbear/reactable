3c89af32f7e1087142f9b7089d64817f83c32706
codbear <douglas.meunier@openclassrooms.com>
2021-11-14 01:06:41 +0100

    feat(Table): add custom style props

--- src/lib/components/Table/Table.jsx
+++ src/lib/components/Table/Table.jsx
@@ -1,6 +1,32 @@
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
 
 import useTable from '../../hooks/useTable';
+import { useMemo } from 'react';
+
+const propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.objectOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number]))
+  ).isRequired,
+
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      dataField: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+
+  variant: PropTypes.oneOf(['filled', 'outlined']),
+
+  color: PropTypes.string,
+
+  headerTextColor: PropTypes.string,
+};
+
+const defaultProps = {
+  variant: 'outlined',
+  color: '#c7c7c7',
+  headerTextColor: '#000000',
+};
 
 const TableWrapper = styled.div`
   width: 100%;
@@ -9,7 +35,7 @@ const TableWrapper = styled.div`
 
 const TableContainer = styled.table`
   width: 100%;
-  border: 1px solid rgba(0, 0, 0, 0.12);
+  border: 1px solid ${({ theme }) => theme.palette.divider};
   border-collapse: collapse;
 `;
 
@@ -17,56 +43,85 @@ const TableRow = styled.tr`
   height: 52px;
 
   &:hover {
-    background-color: #fafafa;
+    background-color: ${({ theme }) => theme.palette.secondary};
   }
 `;
 
-const TableHeaderRow = styled.tr`
+const TableHeaderRowOutlined = styled.tr`
   height: 56px;
+`;
+
+const TableHeaderRowFilled = styled(TableHeaderRowOutlined)`
+  background-color: ${({ theme }) => theme.palette.primary};
 `;
 
 const TableCell = styled.td`
   padding: 6px 16px;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+  border-bottom: 1px solid ${({ theme }) => theme.palette.divider};
 `;
 
 const TableHeaderCell = styled(TableCell)`
+  color: ${({ theme }) => theme.palette.text};
   text-align: left;
   white-space: nowrap;
 `;
 
-const Table = ({ data, columns }) => {
+const variantToTableHeaderRow = {
+  outlined: TableHeaderRowOutlined,
+  filled: TableHeaderRowFilled,
+};
+
+const Table = ({ data, columns, variant, color, headerTextColor }) => {
+  const theme = useMemo(
+    () => ({
+      palette: {
+        primary: color,
+        secondary: '#fafafa',
+        divider: '#e5e8ec',
+        text: headerTextColor,
+      },
+    }),
+    [color, headerTextColor]
+  );
+
   const { header, rows } = useTable(data, columns);
 
-  return (
-    <TableWrapper>
-      <TableContainer>
-        {header && (
-          <thead>
-            {header.rows.map((row) => (
-              <TableHeaderRow {...row.props}>
-                {row.cells.map((cell) => (
-                  <TableHeaderCell as="th" {...cell.props}>
-                    {cell.value}
-                  </TableHeaderCell>
-                ))}
-              </TableHeaderRow>
-            ))}
-          </thead>
-        )}
+  const TableHeaderRow = variantToTableHeaderRow[variant];
 
-        <tbody>
-          {rows.map((row) => (
-            <TableRow {...row.props}>
-              {row.cells.map((cell) => (
-                <TableCell {...cell.props}>{cell.value}</TableCell>
+  return (
+    <ThemeProvider theme={theme}>
+      <TableWrapper>
+        <TableContainer>
+          {header && (
+            <thead>
+              {header.rows.map((row) => (
+                <TableHeaderRow {...row.props}>
+                  {row.cells.map((cell) => (
+                    <TableHeaderCell as="th" {...cell.props}>
+                      {cell.value}
+                    </TableHeaderCell>
+                  ))}
+                </TableHeaderRow>
               ))}
-            </TableRow>
-          ))}
-        </tbody>
-      </TableContainer>
-    </TableWrapper>
+            </thead>
+          )}
+
+          <tbody>
+            {rows.map((row) => (
+              <TableRow {...row.props}>
+                {row.cells.map((cell) => (
+                  <TableCell {...cell.props}>{cell.value}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </tbody>
+        </TableContainer>
+      </TableWrapper>
+    </ThemeProvider>
   );
 };
+
+Table.propTypes = propTypes;
+Table.defaultProps = defaultProps;
 
 export default Table;
