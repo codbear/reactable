87dbf48f0ff9ee02ddccee6d8d91d4eef25ca1b6
codbear <douglas.meunier@openclassrooms.com>
2021-11-27 12:23:12 +0100

    feat(mocks): add data

--- src/lib/mocks/data.js
+++ src/lib/mocks/data.js
@@ -14,6 +14,61 @@ const tableDataMock = [
     name: 'Black Widow',
     age: 26,
   },
+  {
+    secretIdentity: 'Bruce Banner',
+    name: 'Hulk',
+    age: 43,
+  },
+  {
+    secretIdentity: 'Peter Parker',
+    name: 'Spider Man',
+    age: 22,
+  },
+  {
+    secretIdentity: "T'Challa",
+    name: 'Black Panther',
+    age: 34,
+  },
+  {
+    secretIdentity: 'Carole Denvers',
+    name: 'Captain Marvel',
+    age: 27,
+  },
+  {
+    secretIdentity: 'Scott Lang',
+    name: 'Ant Man',
+    age: 29,
+  },
+  {
+    secretIdentity: 'Stephen Strange',
+    name: 'Doctor Strange',
+    age: 49,
+  },
+  {
+    secretIdentity: 'Peter Quill',
+    name: 'Star Lord',
+    age: 32,
+  },
+  {
+    secretIdentity: 'N/A',
+    name: 'Thor',
+    age: 'N/A',
+  },
+  {
+    secretIdentity: 'Wade Wilson',
+    name: 'DeadPool',
+    age: 'N/A',
+  },
+  {
+    secretIdentity: 'Logan',
+    name: 'Wolverine',
+    age: 'N/A',
+  },
+  {
+    secretIdentity: 'Matt Murdock',
+    name: 'Daredevil',
+    age: 44,
+  },
 ];
 
 export default tableDataMock;
