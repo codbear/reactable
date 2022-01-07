b63a92c786172c9f0d41cceb9e0636e3189dc839
codbear <douglas.meunier@openclassrooms.com>
2022-01-07 12:19:00 +0100

    doc(stories): update stories

--- src/stories/Table.stories.jsx
+++ src/stories/Table.stories.jsx
@@ -8,12 +8,17 @@ export default {
   component: Table,
 };
 
-const Template = (args) => <Table {...args} />;
+const Template = (args) => {
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+
+  return <Table itemsPerPage={itemsPerPage} onChangeItemsPerPage={setItemsPerPage} {...args} />;
+};
 
 export const Default = Template.bind({});
 Default.args = {
   data: tableDataMock,
   columns: basicColumnsMock,
+  itemsPerPage: 0,
 };
 
 export const Filled = Template.bind({});
@@ -23,17 +28,26 @@ Filled.args = {
   headerTextColor: '#000000',
   data: tableDataMock,
   columns: basicColumnsMock,
+  itemsPerPage: 0,
 };
 
 export const Filtering = Template.bind({});
 Filtering.args = {
   data: tableDataMock,
   columns: basicColumnsMock,
+  itemsPerPage: 0,
 };
 
-export const Sorting = Template.bind({});
-Sorting.args = {
+export const withSorting = Template.bind({});
+withSorting.args = {
   data: tableDataMock,
   columns: sortingColumnsMock,
-  shouldUseSorting: true,
+  itemsPerPage: 0,
+};
+
+export const withPagination = Template.bind({});
+withPagination.args = {
+  data: tableDataMock,
+  columns: basicColumnsMock,
+  itemsPerPageOptions: [5, 10, 25, 50, 100],
 };
