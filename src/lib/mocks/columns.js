191d18fc2c0887775bebc8a27f4cf29a0784cff5
codbear <douglas.meunier@openclassrooms.com>
2022-01-07 12:15:02 +0100

    feat(mocks): use Faker to create data set

--- src/lib/mocks/columns.js
+++ src/lib/mocks/columns.js
@@ -1,31 +1,84 @@
 export const basicColumnsMock = [
   {
-    header: 'Super Hero',
-    dataField: 'name',
+    header: 'First Name',
+    dataField: 'firstName',
   },
   {
-    header: 'Secret identity',
-    dataField: 'secretIdentity',
+    header: 'Last Name',
+    dataField: 'lastName',
   },
   {
-    header: 'Age',
-    dataField: 'age',
+    header: 'Start date',
+    dataField: 'startDate',
+  },
+  {
+    header: 'Department',
+    dataField: 'department',
+  },
+  {
+    header: 'Date of birth',
+    dataField: 'dateOfBirth',
+  },
+  {
+    header: 'Street',
+    dataField: 'street',
+  },
+  {
+    header: 'City',
+    dataField: 'city',
+  },
+  {
+    header: 'State',
+    dataField: 'state',
+  },
+  {
+    header: 'ZIP code',
+    dataField: 'zipCode',
   },
 ];
 
 export const sortingColumnsMock = [
   {
-    header: 'Super Hero',
-    dataField: 'name',
+    header: 'First Name',
+    dataField: 'firstName',
     isSortable: true,
   },
   {
-    header: 'Secret identity',
-    dataField: 'secretIdentity',
+    header: 'Last Name',
+    dataField: 'lastName',
+    isSortable: true,
   },
   {
-    header: 'Age',
-    dataField: 'age',
+    header: 'Start date',
+    dataField: 'startDate',
+  },
+  {
+    header: 'Department',
+    dataField: 'department',
+    isSortable: true,
+  },
+  {
+    header: 'Date of birth',
+    dataField: 'dateOfBirth',
+  },
+  {
+    header: 'Street',
+    dataField: 'street',
+    isSortable: true,
+  },
+  {
+    header: 'City',
+    dataField: 'city',
+    isSortable: true,
+  },
+  {
+    header: 'State',
+    dataField: 'state',
+    isSortable: true,
+  },
+  {
+    header: 'ZIP code',
+    dataField: 'zipCode',
     isSortable: true,
   },
 ];
--- src/lib/mocks/data.js
+++ src/lib/mocks/data.js
@@ -1,74 +1,26 @@
-const tableDataMock = [
-  {
-    secretIdentity: 'Tony Stark',
-    name: 'Iron Man',
-    age: 38,
-  },
-  {
-    secretIdentity: 'Steve Rogers',
-    name: 'Captain America',
-    age: 90,
-  },
-  {
-    secretIdentity: 'Natalia Romanova',
-    name: 'Black Widow',
-    age: 26,
-  },
-  {
-    secretIdentity: 'Bruce Banner',
-    name: 'Hulk',
-    age: 43,
-  },
-  {
-    secretIdentity: 'Peter Parker',
-    name: 'Spider Man',
-    age: 22,
-  },
-  {
-    secretIdentity: "T'Challa",
-    name: 'Black Panther',
-    age: 34,
-  },
-  {
-    secretIdentity: 'Carole Denvers',
-    name: 'Captain Marvel',
-    age: 27,
-  },
-  {
-    secretIdentity: 'Scott Lang',
-    name: 'Ant Man',
-    age: 29,
-  },
-  {
-    secretIdentity: 'Stephen Strange',
-    name: 'Doctor Strange',
-    age: 49,
-  },
-  {
-    secretIdentity: 'Peter Quill',
-    name: 'Star Lord',
-    age: 32,
-  },
-  {
-    secretIdentity: 'N/A',
-    name: 'Thor',
-    age: 'N/A',
-  },
-  {
-    secretIdentity: 'Wade Wilson',
-    name: 'DeadPool',
-    age: 'N/A',
-  },
-  {
-    secretIdentity: 'Logan',
-    name: 'Wolverine',
-    age: 'N/A',
-  },
-  {
-    secretIdentity: 'Matt Murdock',
-    name: 'Daredevil',
-    age: 44,
-  },
-];
+import faker from 'faker';
+
+const getFakeData = (numberOfEmployees) => {
+  const data = [];
+
+  for (let i = 0; i < numberOfEmployees; i++) {
+    const person = {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      startDate: faker.date.past().toString(),
+      department: faker.name.jobArea(),
+      dateOfBirth: faker.date.past().toString(),
+      street: faker.address.streetAddress(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zipCode: faker.address.zipCode(),
+    };
+    data.push(person);
+  }
+
+  return data;
+};
+
+const tableDataMock = getFakeData(50);
 
 export default tableDataMock;
